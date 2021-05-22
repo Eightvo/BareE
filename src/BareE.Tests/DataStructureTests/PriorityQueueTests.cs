703aa800982f3578318d07eed16d9b905f455806
Eightvo <Eightvo8080@gmail.com>
2021-05-21 22:01:26 -0400

    Fix wellordered test to test well ordered ness and add std ordered test.

--- src/BareE.Tests/DataStructureTests/PriorityQueueTests.cs
+++ src/BareE.Tests/DataStructureTests/PriorityQueueTests.cs
@@ -10,7 +10,7 @@ namespace BareE.Tests.DataStructureTests
     public class PriorityQueueTests
     {
         [TestMethod]
-        public void PriorityQueueTest_WellOrdered()
+        public void PriorityQueueTest_Ordered()
         {
             BareE.DataStructures.PriorityQueue<int> toTest = new DataStructures.PriorityQueue<int>();
             foreach(int i in new List<int>() { 10,3,5,2,5,5,2,6,8,9,2,3,10})
@@ -25,5 +25,24 @@ namespace BareE.Tests.DataStructureTests
                 p = c;
             }
         }
+
+        [TestMethod]
+        public void PriorityQueueTest_WellOrdered()
+        {
+            BareE.DataStructures.PriorityQueue<int> toTest = new DataStructures.PriorityQueue<int>();
+            toTest.Enqueue(3, 5); //Enque Element to receive third.
+            toTest.Enqueue(4, 5); //Enqueue Element to receive fourth.
+            toTest.Enqueue(1, 1); //Enqueue Element to Recieve First
+            toTest.Enqueue(2, 2); //Enqueue Element to receive Second.
+            toTest.Enqueue(5, 5); //Enqueue Element to Receive Fith.
+            Assert.AreEqual(toTest.Dequeue(), 1);
+            Assert.AreEqual(toTest.Dequeue(), 2);
+            Assert.AreEqual(toTest.Dequeue(), 3);
+            Assert.AreEqual(toTest.Dequeue(), 4);
+            Assert.AreEqual(toTest.Dequeue(), 5);
+
+        }
+
+
     }
 }
